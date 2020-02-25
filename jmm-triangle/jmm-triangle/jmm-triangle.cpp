0871528292572c5c8e3c46baf8bcb90d17989497
JasonM2396 <47018044+JasonM2396@users.noreply.github.com>
2020-02-25 10:45:27 -0600

    Traingle upload

---
+++ jmm-triangle/jmm-triangle/jmm-triangle.cpp
@@ -0,0 +1,45 @@
+// jmm-triangle.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// By: Jason Martinez
+// Date: 2/24/20
+// Description: This program will determine if a triangle is a right triangle based on what length the user inputs for each side.
+// Credits and citations for any code used as a reference for this program are listed on line 41.
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+	// Variables
+	float fltSideA = 0.0f;
+	float fltSideB = 0.0f;
+	float fltSideC = 0.0f;
+
+	// User Input
+	cout << "Enter the length of the first side of the triangle: ";
+	cin >> fltSideA;
+	cout << "Enter the length of the second side of the triangle: ";
+	cin >> fltSideB;
+	cout << "Enter the length of the third side of the triangle: ";
+	cin >> fltSideC; 
+	cout << endl;
+
+	fltSideC = pow(fltSideC, 2.0f); // This is placed to added the power subscript to the user input for Side C.
+
+	// Resulting Output
+	cout << "Side A + Side B = " << pow(fltSideA, 2.0f) + pow(fltSideB, 2.0f) << endl;
+	cout << "Side C = " << fltSideC << endl;
+
+	// If the resulting output is or is NOT a right triangle
+	if (fltSideC == pow(fltSideA, 2.0f) + pow(fltSideB, 2.0f)) {
+		cout << "\nThe result is a right triangle!" << endl;
+	}
+	else
+		cout << "\nThe result is NOT a right triangle!" << endl;	
+
+	return 0;
+}
+// Citations and Credits
+// Used some of the Phythagorean Theorem assignment code for the if statements to work.
+// Credits to Matorian from the Cplusplus.com forums for having some code that I looked at as a reference for certain parts, like the if else statement. 
+// Link to that forum: http://www.cplusplus.com/forum/beginner/29025/
