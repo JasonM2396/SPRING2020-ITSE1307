34a5cf850f1105f70b53bd6857540bab0d548e0a
JasonM2396 <47018044+JasonM2396@users.noreply.github.com>
2020-03-31 20:53:07 -0500

    Re-written the code to be more clean and effcient

--- jmm-numbers/jmm-numbers/jmm-numbers.cpp
+++ jmm-numbers/jmm-numbers/jmm-numbers.cpp
@@ -6,41 +6,32 @@
 // Read Citiation and Credits at line 42.
 
 #include <iostream>
-#include <sstream> // To convert the interger to a string.
+#include <string>
 int main()
 {
-	int intNum = 0;
-	int intSum = 0;
-	int intRemainder = 0;
-
-	// User Input
-	std::cout << "Please enter an integer: ";
-	std::cin >> intNum;
-
-	std::stringstream ssNumConv;
-	ssNumConv << intNum;
-	std::string strDigits; // I was unable to get both numbers to display correcting without using strings, so converting the int to a string was the only way.
-	ssNumConv >> strDigits;
-
-	std::cout << std::endl;
-	std::cout << strDigits;
-	intNum = std::abs(intNum); // When the user inputs a negative number, so the sum is not equal to zero.
-	std::cout << std::endl;
-
-	// While loop and output
-	while (intNum > 0)
 	{
-		intRemainder = intNum % 10;
-		intSum = intSum + intRemainder;
-		intNum = intNum / 10;
-	}
+		int intNumber = 3456;
+		int intIndividualDigits = 0;
+		int intSum = 0;
+		std::string strOutput; // Done to prevent the number reversal after input.
+		std::cout << "Please enter a number: ";
+		std::cin >> intNumber;
 
-	std::cout << std::endl;
-	std::cout << "The sum of the number is: " << intSum << std::endl;
+	intNumber = std::abs(intNumber); // This will convert a negative number the user inputs into positive, this prevents the sum equaling to zero.
+
+		while (intNumber != 0)
+		{
+			intIndividualDigits = intNumber % 10;
+			intSum = intSum + intIndividualDigits;
+			intNumber = intNumber / 10;
+			strOutput.insert(0, std::to_string(intIndividualDigits));
+			strOutput.insert(0, " ");
+		}
+
+		std::cout << strOutput << std::endl;
+		std::cout << "The sum of the number is: " << intSum << std::endl;
+		return 0;
+	}
 }
 
-/* Citations and Credits: Javatpoint for helping me on the code for sum of the digits and converting the int to a string for the number display to be correct.
-And pprefernce.com for helping me learn the abs std function: https://en.cppreference.com/w/cpp/numeric/math/fabs
-Javatpoint sites: https://www.javatpoint.com/cpp-int-to-string 
-https://www.javatpoint.com/sum-of-digits-program-in-cpp
-*/
+// Citations and Credits: Cpprefernce.com for helping me learn the abs std function: https://en.cppreference.com/w/cpp/numeric/math/fabs
