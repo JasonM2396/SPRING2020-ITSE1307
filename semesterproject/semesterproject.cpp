a012731512ff1c8a0892631fad521ba7852e0091
JasonM2396 <47018044+JasonM2396@users.noreply.github.com>
2020-05-04 22:07:36 -0500

    Add files via upload
    New version of semester project

---
+++ semesterproject/semesterproject.cpp
@@ -0,0 +1,170 @@
+// semesterproject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+// By: ITSE 1307
+// Date: 20200206
+// Description: This the main for a text based adventure dungeon crawler
+
+#include "stdafx.h"
+//#include "pch.h"
+#include <iostream>
+#include <string>
+#include <time.h>
+#include <random>
+#include "Room.h"
+
+
+
+int determineHighScore(int & intCurrentHighScore, int intPotentialHighScore) {
+	if (intPotentialHighScore >= intCurrentHighScore) {
+		intCurrentHighScore = intPotentialHighScore;
+	}
+	return intCurrentHighScore;
+}
+
+
+int calculateHighScore(int intNumberOfItems, int NumberOfCoins) {
+	//TODO Come up with formula for making high score
+	return intNumberOfItems + NumberOfCoins;
+}
+
+void displaySplashScreen() {
+	// Splash Screen
+	std::cout << "\nSplash Screen";
+	std::cout << "\nGame Title";
+}
+
+void displayMainMenu() {
+	// Main Menu
+	std::cout << "\nMain Menu" << std::endl;
+	std::cout << "s) Start Game" << std::endl;
+
+	std::cout << "q) Quit Game" << std::endl;
+	std::cout << "What would you like to do: ";
+}
+
+
+int main()
+{
+	srand((unsigned int)time(NULL));
+	// Variables
+	int intNumberOfCoins = 0;
+	int intNumberOfItems = 0;
+	int intStartingNumberOfLives = 3;
+	int intNumberOfLives = intStartingNumberOfLives;
+	int intHighScore = 100;
+	char chrMenuChoice = ' ';
+	char chrDirectionChoice = ' ';
+	std::string strAcceptedChoiceInRoom = "wasd0ict";
+	bool boolWinCondition = false;
+
+	displaySplashScreen();
+	
+	do {
+		displayMainMenu();
+
+		std::cin >> chrMenuChoice;
+
+		switch (chrMenuChoice) {
+			case 's':
+				// Setup of game state for new game place Start the game
+				intNumberOfCoins = 0;
+				intNumberOfItems = 0;
+				intNumberOfLives = intStartingNumberOfLives;
+				boolWinCondition = false;
+				std::cout << "You notice that you are inside an abandoned tunnel, which has sat for there abandoned for decades." << std::endl;
+
+				// Main game room movement and search loop
+				while (boolWinCondition != true && intNumberOfLives > 0) {
+					std::cout << "Player Stats: Lives: " << intNumberOfLives << ", Items: " << intNumberOfItems << ", Coins: " << intNumberOfCoins << std::endl;
+					std::cout << "You have entered a room in the tunnel ..." << std::endl;
+
+					
+					Room objRoom = Room();
+					std::cerr << objRoom.toString() << std::endl;
+
+					//Loop for correct user input
+					do {
+						std::cout << "Which direction do you want to go (w [go North], a [go West], s [go South], d [go East])? " << std::endl;
+						std::cout << "If you want to search for coins, items or traps, press these keys (c [Coins], i [items], t[traps])? ";
+						std::cout << "If you want to quit the game, enter zero (0). " << std::endl;
+						std::cin >> chrDirectionChoice;
+					} while (strAcceptedChoiceInRoom.find(chrDirectionChoice) == std::string::npos);
+
+
+					//Logic for handling user input choice
+					switch (chrDirectionChoice) {
+					case 'w': //Walk North
+						std::cout << "You walk north..." << std::endl;
+						objRoom.checkForTrap(intNumberOfLives);
+						break;
+					case 'a': //Walk West
+						std::cout << "You walk west..." << std::endl;
+						objRoom.checkForTrap(intNumberOfLives);
+
+						break;
+					case 's': //Walk South
+						std::cout << "You walk south..." << std::endl;
+						objRoom.checkForTrap(intNumberOfLives);
+
+						break;
+					case 'd': //Walk East
+						std::cout << "You walk east..." << std::endl;
+						objRoom.checkForTrap(intNumberOfLives);
+
+						break;
+					case 'i': //Search for items
+						std::cout << "You search for items in the room...";
+						if (objRoom.checkForItem()) {
+							intNumberOfItems++;
+							std::cout << " you found a rare item!";
+							if (intNumberOfItems >= 2) {
+								boolWinCondition = true;
+							}
+						}
+						else {
+							std::cout << " no items found.";
+						}
+						break;
+					case 'c': //Search for coins
+						std::cout << "You search for coins in the room...";
+						if (objRoom.getCoins() > 0) {
+							intNumberOfCoins += objRoom.getCoins();
+							std::cout << " you found " << objRoom.getCoins() << " coin(s).";
+						}
+						else {
+							std::cout << "no coins are found.";
+						}
+						break;
+					case 't': //Search for traps
+						std::cout << "You search for traps in the room...";
+						if (objRoom.checkForTrap(intNumberOfLives, true)) {
+							// Logic moved into class
+						}
+						else {
+							std::cout << " no traps found.";
+						}
+						break;
+					case '0': //Developers exit menu trick.
+						intNumberOfLives = 0;
+						std::cout << "You gave up" << std::endl;
+						std::cout << "\nYour Score: " << calculateHighScore(intNumberOfItems, intNumberOfCoins);
+						std::cout << "\nHigh Score: " << determineHighScore(intHighScore, calculateHighScore(intNumberOfItems, intNumberOfCoins));
+						break;
+					} // Switch for player choices in rooom
+					std::cout << std::endl;
+				} /// Game Loop
+
+
+				break;
+			case 'q':
+				//TODO: Calculate High Score
+				std::cout << "\nCongratulations, you " << (boolWinCondition ? "won!" : "lost?" );
+				std::cout << "\nYour Score: " << calculateHighScore(intNumberOfItems, intNumberOfCoins);
+				std::cout << "\nHigh Score: " << determineHighScore(intHighScore, calculateHighScore(intNumberOfItems, intNumberOfCoins));
+				break;
+		}
+	} while (chrMenuChoice != 'q');
+
+	std::cout << std::endl;
+
+	return 0;
+}
